bde138b90a646e7466829655afdcdf02100e4f8e
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-01 00:17:18 -0500

    c

--- garbage.php
+++ garbage.php
@@ -9,17 +9,17 @@ if ($con) {
 
     if (isset($_GET['d1'])) {
         $d1 = $_GET['d1'];
-        echo '$d1';
+        echo "Hola que ahce \n '$d1'";
         //echo " Distancia 1: ".$d1;
     }
 
-    if (isset($_POST['d2'])) {
-        $d2 = $_POST['d2'];
+    if (isset($_GET['d2'])) {
+        $d2 = $_GET['d2'];
         //echo " Distancia 2: ".$d2;
     }
 
-    if (isset($_POST['d3'])) {
-        $d3 = $_POST['d3'];
+    if (isset($_GET['d3'])) {
+        $d3 = $_GET['d3'];
         //echo " Distancia 3: ".$d3;
         date_default_timezone_set('america/bogota');
         $fecha_actual = date("Y-m-d H:i:s");
@@ -33,8 +33,8 @@ if ($con) {
             $d3=50;
         }
 
-        $consulta = "INSERT INTO state(state_id_garbage,destance,date) VALUES (1,'$d1','$fecha_actual')," +
-            "(2,'$d2','$fecha_actual'), (3,'$d3','$fecha_actual');";
+        $consulta = "INSERT INTO state(state_id_garbage,destance,date) VALUES (1,'$d1','$fecha_actual'),
+            (2,'$d2','$fecha_actual'), (3,'$d3','$fecha_actual');";
         $resultado = pg_query($con, $consulta);
         $result = print_r($resultado, true);
         echo "\n" . $consulta;

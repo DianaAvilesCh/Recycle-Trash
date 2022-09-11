9e5aa51fd24b4565ee0fa5c07dbeb7c855d80066
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-11 16:48:42 -0500

    Update esp32cam.php

--- controller/esp32cam.php
+++ controller/esp32cam.php
@@ -7,8 +7,11 @@ if ($con) {
 
     echo "Conexion con base de datos exitosa! ";
 
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    echo $data[0];
 
-    if (isset($_POST['json'])) {
+/*     if (isset($_POST['json'])) {
         $d1 = $_POST['json'];
      var_dump(json_decode($d1,true));
      echo $obj -> {'fotografias'};
@@ -23,7 +26,7 @@ if ($con) {
         } else {
             echo " Falla! Registro BD";
         }
-    }
+    } */
 }
 else {
     echo "Falla! conexion con Base de datos ";

18e119af3f6c8515d6a862b39ecaf11925dbd66b
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-13 15:34:24 -0500

    insert

--- controller/prueba.php
+++ controller/prueba.php
@@ -1,46 +1,15 @@
 <?php
 
-include 'conexion.php';
-
-if ($con) {
-
-
   echo "Conexion con base de datos exitosa! ";
- if (file_get_contents('php://input')) {
-    $json = file_get_contents('php://input');
-    $data = json_decode($json, true);
-    echo "aqui llegue";
-    //echo $data['fotografias'];
-    //echo file_get_contents('php://input');
-    $data = $data['fotografias'];
 
-    list($type, $data) = explode(';', $data);
-    list(, $data)      = explode(',', $data);
-    $data = base64_decode($data);
-    $DateAndTime = (string) date('mdY-his', time());
-    $nomimg = "../capture/images$DateAndTime.png";
-    file_put_contents($nomimg, $data);
-
-    $sql = "INSERT INTO public.capture(url)
-      VALUES ('$nomimg');";
-    echo $sql;
-    $resultado = pg_query($con, $sql);
-    echo $resultado;
-    if ($resultado) {
-      echo 'si guarde';
-    }
-
-    //DE AQUI
-/*
 ?>
     <!-- Load TensorFlow.js. This is required to use MobileNet. -->
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.1"> </script>
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet"> </script>
+    <script src="../Modelo/model2.json"> </script>
     <!-- Load the MobileNet model. -->
 
     <!-- Replace this with your image. Make sure CORS settings allow reading the image! -->
-    <img id="img1" src="<?php echo $nomimg ?>"></img>
-    <!--<img id="img1" src="../capture/images09132022-064557.png"></img>-->
+    <img id="img1" src="../capture/images09132022-085916.png"></img>
     <div id="results" />
 
     <!-- Place your code in the script tag below. You can also use an external .js file -->
@@ -57,7 +26,7 @@ if ($con) {
         var clase = '';
         img.onload = function() {
           console.log('Wait to load..');
-          mobilenet.load().then(model => {
+          model2.json.load().then(model => {
             // Classify the image.
             model.classify(img).then(predictions => {
               for (i in predictions) {
@@ -75,16 +44,8 @@ if ($con) {
             });
           });
         }
-        //img.src ="../capture/images09132022-081634.png";
       }
     </script>
 <?php
-*/ //HASTA AQUI
-//unlink("$nomimg");
-  }
-  //unlink("../capture/images09132022-043241.png");
-} else {
-  echo "Falla! conexion con Base de datos ";
-}
 
 ?>

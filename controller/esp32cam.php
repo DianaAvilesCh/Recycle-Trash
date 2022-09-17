1f485b0917be50f7538573e8a3583e3404f861a0
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-17 17:40:45 -0500

    cosas

--- controller/esp32cam.php
+++ controller/esp32cam.php
@@ -1,17 +1,10 @@
 <?php
-
 include 'conexion.php';
-
 if ($con) {
-
-  echo "Conexion con base de datos exitosa! ";
+  //if post of arduino
  if (file_get_contents('php://input')) {
-  echo "\nIngerso correcto al post";
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
-    echo "\naqui llegue";
-    //echo $data['fotografias'];
-    //echo file_get_contents('php://input');
     $data = $data['fotografias'];
 
     list($type, $data) = explode(';', $data);
@@ -21,19 +14,55 @@ if ($con) {
     $nomimg = "../capture/images$DateAndTime.png";
     file_put_contents($nomimg, $data);
 
-    echo $nomimg;
-
-    $sql = "INSERT INTO capture(url) VALUES('$nomimg');";
-    $resultado = pg_query($con, $sql);
-    if ($resultado) {
-        echo "\nGuardado correctamente";
-    } else {
-        echo "\nincorrecto no se guardo";
-    }
+    ?>
+    <!-- Load TensorFlow.js.-->
+    </br>
+    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.20.0/dist/tf.min.js"></script>
+    
+    <!-- Replace this with your image. Make sure CORS settings allow reading the image! -->
+    <img id="img1" crossorigin='anonymous' src="<?php $nomimg?>"></img>
+    <div id="results" />
+    
+    <script>
+      async function init() {
+    
+        //model =  await tf.loadModel('indexeddb://my-model-1');
+        model = await tf.loadGraphModel('../Model/model.json');
+    
+        //model =  await tf.loadModel('tfjsversion/model.json');
+        console.log('model loaded from storage');
+    
+        input = document.getElementById("img1");
+        const tensor = await tf.browser.fromPixels(input).resizeNearestNeighbor([256,256]).toFloat()
+        pred = model.predict(tf.reshape(tensor, [1, 256, 256, 3]))
+        pred.print()
+        console.log("End of predict function")
+        //This array is encoded with index i = corresponding emotion. In dataset, 0 = Angry, 1 = Disgust, 2 = Fear, 3 = Happy, 4 = Sad, 5 = Surprise and 6 = Neutral
+        category = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+        //At which index in tensor we get the largest value ?
+        pred.data()
+          .then((data) => {
+            console.log(data)
+            output = document.getElementById("results")
+            output.innerHTML = ""
+            max_val = -1
+            max_val_index = -1
+            for (let i = 0; i < data.length; i++) {
+              if (data[i] > max_val) {
+                max_val = data[i]
+                max_val_index = i
+              }
+            }
+            CATEGORY_DETECTED = category[max_val_index]
+            output.innerHTML = CATEGORY_DETECTED;
+            <?php echo"Falla! conexion con Base de datos ";?>
+            })
+      }
+      init();
+    </script>
+    <?php
 
   }//fin del if post
-
-  
 
 } else {//else de si falla conexion
   echo "Falla! conexion con Base de datos ";

1473b09bb94a2e28ad07c97eff0fd948ba058958
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-19 18:29:16 -0500

    Update d1.php

--- controller/d1.php
+++ controller/d1.php
@@ -7,9 +7,9 @@ $resultado = pg_query($con, $sql);
 if (pg_num_rows($resultado)) {
     while ($obj = pg_fetch_object($resultado)) {
         $dato = $obj->state;
+        echo $obj->state;
     }
 }
-echo $obj->state;
 
 if($dato != "nada"){ 
     $sql = "UPDATE capture SET state='nada';";

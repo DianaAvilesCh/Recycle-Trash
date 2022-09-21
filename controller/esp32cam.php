84f57fa848bfe41f52eb1afdd2cf3c277da31152
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-20 22:33:54 -0500

    Update esp32cam.php

--- controller/esp32cam.php
+++ controller/esp32cam.php
@@ -1,4 +1,6 @@
 <?php
+
+include 'conexion.php';
   //if post of arduino
     if (file_get_contents('php://input')) {
     $json = file_get_contents('php://input');
@@ -10,13 +12,12 @@
     $data = base64_decode($data);
     $DateAndTime = (string) date('mdY-his', time());
     $nomimg = "../capture/images$DateAndTime.png";
-    file_put_contents($nomimg, $data);   
+    file_put_contents($nomimg, $data);  
+    
+    $consulta = "INSERT INTO image(url) VALUES ('$nomimg');";
+    $resultado = pg_query($con, $consulta);
    //$nomimg = "../capture/images09142022-071619.png";
+   header("Location: https://recycle-trash.herokuapp.com/controller/prediction.php?image=$nomimg");
+   exit();
    }
 ?>
-<script type="text/javascript">
-  postimg('<?php echo $nomimg?>');
-    function postimg(img){
-      window.location.href ="prediction.php?image=" + img;
-    }
-</script>

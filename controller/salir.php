ed4bcc3bd374c32524c7b359916a237bd058ecf2
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-25 23:47:47 -0500

    Update salir.php

--- controller/salir.php
+++ controller/salir.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-if ($_SESSION["newsession"] != "nothing" || $_SESSION["newsession"] != null) {
     session_destroy();
     header("Status: 301 Moved Permanently");
     header("Location: ../../index.php");
     exit;
-}
 ?>

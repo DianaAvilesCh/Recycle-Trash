70a87d9a2c6bfdfff226cece1374e1f315b7426d
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2022-09-25 22:31:04 -0500

    sdfsdfdsf

--- activation.php
+++ activation.php
@@ -1,6 +1,7 @@
 <?php
 include('./controller/conexion.php');
 if ($con) {
+    echo "si llego";
     if (isset($_GET['mail'])) {
         $mail = $_GET['mail'];
     }
@@ -9,14 +10,14 @@ if ($con) {
 
         $mailhash = md5($mail);
 
-        $consulta = "SELECT email FROM person WHERE email_hash = $mail;";
+        $consulta = "SELECT email FROM person WHERE email_hash = '$mail';";
         $resultado = pg_query($con, $consulta);
         if (pg_num_rows($resultado)) {
             while ($obj = pg_fetch_object($resultado)) {
                 $dato = $obj->email;
             }
             if (md5($dato) == $mail) {
-                $consulta = "UPDATE person SET state = 1 WHERE email = $dato; ";
+                $consulta = "UPDATE person SET state = 1 WHERE email = '$dato'; ";
                 $resultado = pg_query($con, $consulta);
                 if ($resultado) {
 ?>
